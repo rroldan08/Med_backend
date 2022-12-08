3ba77dd04b4d82ab22e24e3540a9d8de7aa2a82e
angel zambrano <angeldzambrano99@gmail.com>
2022-12-08 15:58:29 -0800

    we now return the current date medicines

--- project/med_tracker/timeline/serializers.py
+++ project/med_tracker/timeline/serializers.py
@@ -4,6 +4,7 @@ from meds.models import DaysOfWeek
 from rest_framework import serializers
 
 from meds.models import medicine_to_daysOfWeek
+from datetime import date
 
 
 class MedicineTimelineSerializer(serializers.ModelSerializer):
@@ -14,20 +15,58 @@ class MedicineTimelineSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User_Medicine
-        fields = '__all__'
+        fields = ('fullday', 'date','timeline',)
 
     def get_timeline(self, obj):
+        today = date.today()
+        x = today.isoweekday()
+        type = {2:'Every other day', 1:'Daily'}
         rr = []
         for an in obj:
-            nObj = {}
-            nObj['id'] = an.usermed_id
-            nObj['name'] = an.name
-            nObj['time'] = an.time
-            rr.append(nObj)
+
+            # getting the days
+            if an.type.med_typeID == 2:
+                nObj = {}
+                nObj['id'] = an.usermed_id
+                nObj['name'] = an.name
+                nObj['time'] = an.time
+                nObj['type'] = type[an.type.med_typeID]
+                nObj['quantity'] = 1
+                nObj['taken'] = False
+                isValid = False
+
+                days = []
+                # get all of the days that the medicine has
+                dayObjs = medicine_to_daysOfWeek.objects.filter(med=an.usermed_id)
+                for day in dayObjs:
+                    if day.day.day_id == x:
+                        isValid = True
+                    days.append(day.day.name)
+                nObj['days'] = days
+                if isValid:
+                    rr.append(nObj)
+
+            else:
+                nObj = {}
+                nObj['id'] = an.usermed_id
+                nObj['name'] = an.name
+                nObj['time'] = an.time
+                nObj['type'] = type[an.type.med_typeID]
+                nObj['quantity'] = 1
+                nObj['taken'] = False
+                rr.append(nObj)
+
+
+
         return rr
 
     def get_fullday(self, obj):
-        return "Thursday"
+        days = {1:"Monday", 2: "Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday", 7:"Sunday"}
+        today = date.today()
+        x = today.isoweekday()
+        return days[x]
 
     def get_date(self, obj):
-        return  "december 8"
+        today = date.today()
+        d2 = today.strftime("%B %d")
+        return  d2
